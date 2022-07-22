3a036516d2ee2c822549ee983efffec8fdf814c3
alsurrusca <alissia.surrusca@gmail.com>
2022-07-22 11:00:39 +0200

    Fichier JSON ok

---
+++ SafetyNetApi/src/main/java/com/openclassrooms/SafetyNetApi/model/Person.java
@@ -0,0 +1,114 @@
+package com.openclassrooms.SafetyNetApi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+
+import javax.persistence.Entity;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+@Entity
+public class Person {
+
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+
+
+
+    private String email;
+
+
+    public Person(){}
+
+
+    public Person(String firstName, String lastName, String address, String city, String zip, String phone, String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+
+    }
+    public String getFirstName() {
+        return firstName;
+
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
+
