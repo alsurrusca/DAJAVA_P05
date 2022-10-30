c20254b7f2fc6837d3a144957301388e5aaba6bb
alsurrusca <57980516+alsurrusca@users.noreply.github.com>
2022-10-30 09:49:05 +0100

    Update Version 1.2

--- SafetyNetApi/src/main/java/com/openclassrooms/SafetyNetApi/controller/URLController.java
+++ SafetyNetApi/src/main/java/com/openclassrooms/SafetyNetApi/controller/URLController.java
@@ -6,7 +6,6 @@ import com.openclassrooms.SafetyNetApi.service.UrlService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +24,7 @@ public class URLController {
     //Manque les habitants de la maison + doublon
     @GetMapping(value = "/childAlert")
     public List<ChildAlertDTO> getChildListByAddress(@RequestParam(value = "address") String address) {
-        List<ChildAlertDTO> childAlertDTOS = urlService.getChildListByAddress(address);
+        List<ChildAlertDTO> childAlertDTOS = urlService.getChildListByAdress(address);
         if (childAlertDTOS.isEmpty()) {
             log.error("Find all child by home request FAILED, address not found or no child found at this address");
         } else {
@@ -37,7 +36,7 @@ public class URLController {
     //OK
     @GetMapping(value = "/personInfo")
     public List<PersonInfoDTO> getPersonInfo(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName) {
-        List<PersonInfoDTO> personInfoDTOS = urlService.getPersonInfo(firstName, lastName);
+        List<PersonInfoDTO> personInfoDTOS = urlService.getPersonListInfo(firstName, lastName);
         if (personInfoDTOS.isEmpty()){
             log.error("Find person info request FAILED, check firstname and lastname");
         } else {
@@ -51,7 +50,7 @@ public class URLController {
     //OK
     @GetMapping(value = "/phoneAlert")
     public PhoneAlertDTO getPhoneAlert(@RequestParam(value = "firestation") String station) {
-        PhoneAlertDTO phoneAlertDTO = urlService.getPhoneNumberFromeStation(station);
+        PhoneAlertDTO phoneAlertDTO = urlService.getPhoneNumberListByStation(station);
         if (phoneAlertDTO.getPhoneAlertList().isEmpty()){
             log.error("Find all phone number by firestation number request FAILED, station number not found");
         } else {
@@ -75,7 +74,7 @@ public class URLController {
     //OK
     @GetMapping(value = "/firestation")
     public PersonListByStationDTO getPersonByListFromStation(@RequestParam(value = "stationNumber") String station) {
-        PersonListByStationDTO personsByStationDTO = urlService.getPersonFromStationNumber(station);
+        PersonListByStationDTO personsByStationDTO = urlService.getPersonFromStation(station);
         if (personsByStationDTO.getPersonByStation().isEmpty()) {
             log.error("Find all person by station number request FAILED");
         } else {
@@ -87,7 +86,7 @@ public class URLController {
     //OK
     @GetMapping(value = "/communityEmail")
     public CommunityEmailDTO getEmailFromAddress(@RequestParam(value = "city") String city) {
-        CommunityEmailDTO communityEmailDTO = urlService.getListFromMail(city);
+        CommunityEmailDTO communityEmailDTO = urlService.getCommunityEmailList(city);
         if (communityEmailDTO.getMailAlert().isEmpty()){
             log.error("Find email by city request FAILED, check the city");
         } else {
@@ -100,7 +99,7 @@ public class URLController {
 
     @GetMapping(value = "/flood/stations")
     public List<AddressDTO> getHomeByStation(@RequestParam(value = "stations") List<String> station) {
-        List<AddressDTO> addressDTOS = urlService.getHomeByStation(station);
+        List<AddressDTO> addressDTOS = urlService.getHomeByStationNumber(station);
         if (addressDTOS.isEmpty()){
             log.error("Find homes by firestation number request FAILED, station number doesn't exist");
         } else {
